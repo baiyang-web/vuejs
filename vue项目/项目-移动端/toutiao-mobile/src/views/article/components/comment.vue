e6aa820ebcab59ba163a66cab35eb5a02a8d413b
baiyang <1786279668@qq.om>
2020-03-24 18:48:57 +0800

    评论列表布局与渲染

--- vue项目/项目-移动端/toutiao-mobile/src/views/article/components/comment.vue
+++ vue项目/项目-移动端/toutiao-mobile/src/views/article/components/comment.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="comment">
     <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <div class="item van-hairline--bottom van-hairline--top" v-for="index in 5" :key="index">
+      <div class="item van-hairline--bottom van-hairline--top" v-for="comment in comments" :key="comment.com_id.toString()">
         <van-image
           round
           width="1rem"
           height="1rem"
           fit="fill"
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
+          :src="comment.aut_photo"
         />
         <div class="info">
           <p>
-            <span class="name">一阵清风</span>
+            <span class="name">{{comment.aut_name}}</span>
             <span style="float:right">
               <span class="van-icon van-icon-good-job-o zan"></span>
-              <span class="count">10</span>
+              <span class="count">{{comment.like_count}}</span>
             </span>
           </p>
-          <p>评论的内容，。。。。</p>
+          <p>{{comment.content}}</p>
           <p>
-            <span class="time">两天内</span>&nbsp;
-            <van-tag plain @click="showReply=true">4 回复</van-tag>
+            <span class="time">{{comment.pubdate | relTime}}</span>&nbsp;
+            <!-- 点击回复标签 弹出面板 -->
+            <van-tag plain @click="showReply=true">{{comment.reply_count}} 回复</van-tag>
           </p>
         </div>
       </div>
@@ -31,6 +32,7 @@
         <span class="submit" v-else slot="button">提交</span>
       </van-field>
     </div>
+    <!-- 放置评论的评论 弹出面板 -->
   </div>
 
   <!-- 都不输入框 -->
